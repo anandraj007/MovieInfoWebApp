bc52a404468344c53cfd36b517c8a3891adf8a04
Anand Raj <ananduiitp07@gmail.com>
2019-09-23 00:03:42 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText){
+  axios.get('http://www.omdbapi.com?s='+ searchText+'&apikey=58e67be')  
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#"><img src="${movie.Poster}"></a>
+              <h5 style="color:white;">${movie.Title}</h5>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie(){
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=58e67be')
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2 style="color:white;">${movie.Title}</h2>
+            <ul class="list-group">
+              <h2 class="text-center"> Movie Details</h2>
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+      `;
+
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
